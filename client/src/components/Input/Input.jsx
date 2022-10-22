f09cd39e99c72138712e892b5a93cd76b3f94694
manoj <manojsrinivasan1999k@gmail.com>
2022-10-22 09:45:13 +0530

    server-client

---
+++ client/src/components/Input/Input.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Input.scss'
+
+
+
+
+const Input = props => {
+ 
+ return <div>
+  <textarea className="form-input" id="" cols="30" rows="1"></textarea>
+  <button>send</button>
+ </div>
+}
+
+export default Input
---
+++ client/src/components/Message/Message.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+
+const Message = props => {
+
+ return <>
+  <div className="messagess">
+   message
+  </div>
+ </>
+}
+
+export default Message;
