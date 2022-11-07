8350a72f4be5648b1dcfeaf746ee675ed52212d1
manoj <43775179+manojDuke-asWebdev@users.noreply.github.com>
2022-11-07 12:05:16 +0530

    server-client set

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(
+ server,
+ {
+  cors: {
+   origin: 'http://localhost:3000',
+   methods: ['GET', 'POST']
+  }
+ }
+)
+
+
+server.listen(3001, () => {
+  console.log('server running')
+})
+
+// socket connection
+class socket {
+  constructor () {
+   this.client()
+  }
+  client = () => {
+    io.on('connection', socket => {
+      
+    })
+  }
+}
+
+const obj = new socket()
--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import io from 'socket.io-client'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// socket 
+const socket = io.connect('http://localhost:3001')
+
+const App = props => {
+ const sendMsg = () => {
+  socket.emit('veify', "hey here I am")
+ }
+ return <>
+   <button onClick={sendMsg}> hello  </button>
+ </>
 }
 
 export default App;
